a0f25446ea59b2729c724103a8eafeeb90287324
Nidula Gunawardana <94860959+nidulaguna@users.noreply.github.com>
2024-01-11 19:24:22 +0530

    another two projects done

---
+++ Projects/pestsense.php
@@ -0,0 +1,291 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content>
+    <meta name="author" content>
+
+    <title>Project - Tesseract</title>
+    <link rel="icon" href="../images/tit.png" />
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap-icons.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="../css/slick.css" />
+
+    <link href="../css/tooplate-little-fashion.css" rel="stylesheet">
+    <link href="../css/hederCoustom.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <main>
+
+        <nav class="navbar sticky-top navbar-expand-lg start-header start-style">
+            <div class="container sticky-top mt-3 mb-3">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <a class="navbar-brand" href="index.html">
+                    <a href="index.php"><img src="../images/Spark light.png" alt="Spark logo" style="height: 50px;"></a>
+                </a>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="../index.php">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="../raspberryPi.php">Raspberry Pi Community</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="../communityFb.php">Community Facebook
+                                Page</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="../softSkills.php">Soft Skills</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="../projects.php">Projects</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+
+        <header class="site-header section-padding-img site-header-image mt-0 tile-color pb-2">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-12 text-center">
+                        <div class="row h-100">
+                            <h2 class="my-auto">
+                                <span class="d-block text-light">Revolutionizing Agriculture<br>
+                                    PestSense - A Sustainable Solution for Insect Management</span>
+                            </h2>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="row">
+                    <div class="col-12 h-100">
+                        <div class="px-5 pt-lg-3 text-center my-auto">
+                            <p class="lead mt-3 fs-4 text-secondary">In the realm of food production and preservation, the excessive and improper use of
+                                insecticides has been a persistent challenge. This misuse not only affects the farming
+                                communities and consumers but also poses a severe threat to ecosystems, leading to
+                                increased production costs, compromised health and safety, and environmental pollution.
+                                The current practices, driven by a lack of knowledge among farmers and the inability to
+                                identify pests before crops are damaged, contribute to significant issues in the agricultural
+                                landscape.</p>
+                        </div>
+                    </div>
+                </div>
+
+
+        </header>
+
+
+
+        <section class="testimonial section-padding pt-2">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">The Problem at Hand</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">Studies reveal alarming statistics - one-third of farmers routinely use insecticides, and 40%
+                            apply pesticides even before pest symptoms appear. This misuse not only harms crop yield
+                            but also results in environmental pollution, especially water bodies showing elevated
+                            pesticide levels beyond permissible thresholds.
+                            Heavy pesticide usage has detrimental effects on water bodies, impacting biodiversity and
+                            causing the death of fish and other organisms. The consequences extend to human health,
+                            with drinking water containing dangerous substances that may lead to various health issues
+                            upon consumption. Direct contact with vegetables and grains also introduces heavy metals,
+                            further amplifying health risks.
+                            Apart from health concerns, the misuse of insecticides contributes to insecticide resistance,
+                            reducing their effectiveness. This, in turn, exacerbates the damage to crops and hinders
+                            food production.</p>
+
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">Introducing PestSense - A Holistic Solution</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">Recognizing the urgency of addressing these issues, PestSense emerges as a
+                            groundbreaking solution. The system is designed to identify insects damaging crops and
+                            recommend precise insecticide usage, thus minimizing environmental impact and promoting
+                            sustainable agriculture.</p>
+
+                    </div>
+                </div>
+
+
+
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">Understanding the Root Causes</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">PestSense addresses the misuse of insecticides by delving into the reasons behind it.
+                            Farmers often expect 100% protection from insecticides, succumb to the pressure for
+                            immediate results, and lack specific instructions for their fields. The absence of training on
+                            chemical usage and limited knowledge on the effects of insecticide misuse further
+                            exacerbate the problem.</p>
+
+                    </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">How PestSense Works</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">At the core of PestSense lies an advanced insect monitoring and management system
+                            equipped with sophisticated traps and sensors. These traps, strategically placed based onfield size and geographical factors, effectively capture targeted insect species. The collected
+                            data, processed through statistical models, provides precise population information.
+                            For insecticide selection, PestSense leverages government and research databases,
+                            ensuring up-to-date information on appropriate types and dosages. The system utilizes a
+                            radio network, powered by a device like Raspberry Pi, to establish real-time connections
+                            between traps and a centralized hub. A user-friendly application accompanies the product,
+                            offering an intuitive interface for visualizing and analyzing collected data.</p>
+
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">Validating the Solution</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">PestSense addresses the root causes of insecticide misuse, providing farmers with specific
+                            information on required chemicals and dosages. This not only saves costs but also
+                            streamlines pest management, promoting the implementation of Integrated Pest
+                            Management without the need for extensive training. The solution constantly updates
+                            farmers on best practices, reducing the overall use of chemicals and minimizing health
+                            impacts.</p>
+
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">Embracing Sustainability</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">PestSense aligns with Sustainable Development Goals (SDGs) by promoting responsible
+                            consumption and production. By reducing insecticide use, the system contributes to climate
+                            action, preserves biodiversity, and supports partnerships for sustainable agriculture.</p>
+
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">Social and Environmental Impact</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">The positive impacts of PestSense extend to society and the environment. Farmers benefit
+                            from accurate data, consumers enjoy safer and healthier food, and local communities
+                            witness reduced environmental impact. Biodiversity is preserved, soil health is enhanced,
+                            and chemical exposure is minimized, contributing to a healthier and more sustainable
+                            agricultural ecosystem.</p>
+
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">Conclusion</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">PestSense is not just a technological marvel; it is a game-changer in sustainable agriculture.
+                            By providing farmers with precise information, the system empowers them to make informed
+                            decisions, ultimately reducing the reliance on harmful insecticides. As we navigate the
+                            challenges of modern agriculture, PestSense emerges as a beacon of hope, ushering in an
+                            era of responsible and sustainable pest management.
+                            In embracing PestSense, we take a significant step towards a future where agriculture
+                            thrives without compromising the health of our communities and the well-being of our planet.
+                            With a focus on responsible agricultural practices, PestSense paves the way for a more
+                            sustainable and harmonious relationship between humanity and the environment.</p>
+
+                    </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">Team</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">Moshintha Hewavitharana, Gishan Damindu, Mewan Rathnayake, Senul Samarasekara</p>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-10 me-auto mb-4">
+                    <h4 class="text-white mb-3"><a href="index.html">SPARK</a> Challenge</h4>
+                    <p class="copyright-text text-muted mt-lg-5 mb-4 mb-lg-0">Copyright © 2023 <strong>ENTC</strong></p>
+                    <br>
+                    <p class="copyright-text">Designed by <a href="https://www.linkedin.com/in/nidula-gunawardana-22819a1b3/" target="_blank">Nidula Gunawardana</a></p>
+                </div>
+
+                <div class="col-lg-5 col-8">
+                    <h5 class="text-white mb-3">Sitemap</h5>
+
+                    <ul class="footer-menu d-flex flex-wrap">
+                        <li class="footer-menu-item"><a href="../index.php" class="footer-menu-link">Home</a></li>
+
+                        <li class="footer-menu-item"><a href="../raspberryPi.php" class="footer-menu-link">Raspberry Pi Community</a></li>
+
+                        <li class="footer-menu-item"><a href="../communityFb.php" class="footer-menu-link">Community Facebook Page</a></li>
+
+                        <li class="footer-menu-item"><a href="../softSkills.php" class="footer-menu-link">Soft Skills</a></li>
+
+                        <li class="footer-menu-item"><a href="../projects.php" class="footer-menu-link">Projects</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-4">
+                    <h5 class="text-white mb-3">Social</h5>
+
+                    <ul class="social-icon">
+
+                        <li><a href="#" class="social-icon-link bi-youtube"></a></li>
+
+                        <li><a href="#" class="social-icon-link bi-whatsapp"></a></li>
+
+                        <li><a href="#" class="social-icon-link bi-instagram"></a></li>
+
+                        <li><a href="#" class="social-icon-link bi-skype"></a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+
+    <!-- JAVASCRIPT FILES -->
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/Headroom.js"></script>
+    <script src="../js/jQuery.headroom.js"></script>
+    <script src="../js/slick.min.js"></script>
+    <script src="../js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ Projects/techstatic.php
@@ -0,0 +1,236 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content>
+    <meta name="author" content>
+
+    <title>Project - Techstatic</title>
+    <link rel="icon" href="../images/tit.png" />
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap-icons.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="../css/slick.css" />
+
+    <link href="../css/tooplate-little-fashion.css" rel="stylesheet">
+    <link href="../css/hederCoustom.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <main>
+
+        <nav class="navbar sticky-top navbar-expand-lg start-header start-style">
+            <div class="container sticky-top mt-3 mb-3">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <a class="navbar-brand" href="index.html">
+                    <a href="index.php"><img src="../images/Spark light.png" alt="Spark logo" style="height: 50px;"></a>
+                </a>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="../index.php">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="../raspberryPi.php">Raspberry Pi Community</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="../communityFb.php">Community Facebook
+                                Page</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="../softSkills.php">Soft Skills</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="../projects.php">Projects</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+
+        <header class="site-header section-padding-img site-header-image mt-0 tile-color pb-2">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-12 text-center">
+                        <div class="row h-100">
+                            <h2 class="my-auto">
+                                <span class="d-block text-light">Techstatic - Innovating Agriculture for Sustainability</span>
+                            </h2>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="row">
+                    <div class="col-12 h-100">
+                        <div class="px-5 pt-lg-3 text-center my-auto">
+                            <p class="lead mt-3 fs-4 text-secondary">Techstatic addresses critical challenges in agriculture through their innovative project.
+                                Focused on the sustainable goal, food production and preservation, their solution tackles
+                                issues ranging from environmental degradation to economic losses for farmers.</p>
+                        </div>
+                    </div>
+                </div>
+
+
+        </header>
+
+
+
+        <section class="testimonial section-padding pt-2">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">Identifying the Problem</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">The team identified excessive fertilizer use as a major problem, leading to soil degradation
+                            and environmental pollution. Additionally, they recognized the lack of precise monitoring
+                            contributing to substantial food wastage during harvesting and processing stages. This
+                            multifaceted problem negatively impacts farmers, food suppliers, the government, residents
+                            in farming areas, and food consumers.</p>
+
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">Comprehensive Digital Solution</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">To address these issues, Techstatic proposes a comprehensive digital system consisting of
+                            a portable sensory device and a mobile app. The sensory device measures crucial soil
+                            parameters such as N, P, K levels, soil moisture, temperature, humidity, and pH. These
+                            readings, combined with user inputs and external data, are sent to a cloud database for
+                            analysis.</p>
+
+                    </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">Intelligent Mobile App</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">The mobile app serves as an intelligent assistant, providing farmers with personalized
+                            recommendations on fertilizer usage, suitable crops, and land management based on sensor
+                            data. It also incorporates a machine learning model to predict crop supply, helping farmers
+                            avoid surplus production. Furthermore, the app includes features for disease identification
+                            using deep learning, real-time alerts, news updates, and educational resources.</p>
+
+                    </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">Positive Feedback</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">The team conducted thorough validation of their solution by consulting agricultural experts,
+                            farmers, and agricultural service officers. The feedback received affirms the practicality and
+                            potential positive impact of their solution.</p>
+
+                    </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">Alignment with Sustainable Development Goals</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">Techstatic's project aligns with SPARK’s mission to create sustainable solutions. It
+                            contributes to various UN Sustainable Development Goals, including sustainable
+                            consumption and production, zero hunger, decent work and economic growth, climate
+                            action, and good health and well-being.</p>
+
+                    </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-12 col-lg-10 offset-lg-1">
+                        <h4 class="text-white">Conclusion</h4>
+                        <p class="lead mb-3 text-secondary my-auto text-justify">Techstatic's project stands as a testament to their commitment to creating a positive impact
+                            in agriculture, aligning with the principles of the SPARK competition for creating a better
+                            sustainable world.</p>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-10 me-auto mb-4">
+                    <h4 class="text-white mb-3"><a href="index.html">SPARK</a> Challenge</h4>
+                    <p class="copyright-text text-muted mt-lg-5 mb-4 mb-lg-0">Copyright © 2023 <strong>ENTC</strong></p>
+                    <br>
+                    <p class="copyright-text">Designed by <a href="https://www.linkedin.com/in/nidula-gunawardana-22819a1b3/" target="_blank">Nidula Gunawardana</a></p>
+                </div>
+
+                <div class="col-lg-5 col-8">
+                    <h5 class="text-white mb-3">Sitemap</h5>
+
+                    <ul class="footer-menu d-flex flex-wrap">
+                        <li class="footer-menu-item"><a href="../index.php" class="footer-menu-link">Home</a></li>
+
+                        <li class="footer-menu-item"><a href="../raspberryPi.php" class="footer-menu-link">Raspberry Pi Community</a></li>
+
+                        <li class="footer-menu-item"><a href="../communityFb.php" class="footer-menu-link">Community Facebook Page</a></li>
+
+                        <li class="footer-menu-item"><a href="../softSkills.php" class="footer-menu-link">Soft Skills</a></li>
+
+                        <li class="footer-menu-item"><a href="../projects.php" class="footer-menu-link">Projects</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-4">
+                    <h5 class="text-white mb-3">Social</h5>
+
+                    <ul class="social-icon">
+
+                        <li><a href="#" class="social-icon-link bi-youtube"></a></li>
+
+                        <li><a href="#" class="social-icon-link bi-whatsapp"></a></li>
+
+                        <li><a href="#" class="social-icon-link bi-instagram"></a></li>
+
+                        <li><a href="#" class="social-icon-link bi-skype"></a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+
+    <!-- JAVASCRIPT FILES -->
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/Headroom.js"></script>
+    <script src="../js/jQuery.headroom.js"></script>
+    <script src="../js/slick.min.js"></script>
+    <script src="../js/custom.js"></script>
+
+</body>
+
+</html>
