f739ab2f53c23f6e781bfb75cf1dd77a3e9328ce
Nidula Gunawardana <94860959+nidulaguna@users.noreply.github.com>
2024-01-11 17:28:52 +0530

    project page compleated

---
+++ projects.php
@@ -0,0 +1,286 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content>
+    <meta name="author" content>
+
+    <title>SPARK Soft Skills</title>
+    <link rel="icon" href="images/tit.png" />
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/slick.css" />
+
+    <link href="css/tooplate-little-fashion.css" rel="stylesheet">
+    <link href="css/hederCoustom.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <main>
+
+        <nav class="navbar sticky-top navbar-expand-lg start-header start-style">
+            <div class="container sticky-top mt-3 mb-3">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <a class="navbar-brand" href="index.html">
+                    <a href="index.php"><img src="images/Spark light.png" alt="Spark logo" style="height: 50px;"></a>
+                </a>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="index.php">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="raspberryPi.php">Raspberry Pi Community</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="communityFb.php">Community Facebook
+                                Page</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="softSkills.php">Soft Skills</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="projects.php">Projects</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+
+        <header class="site-header section-padding-img site-header-image mt-0 tile-color">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-12 header-info text-center">
+                        <div class="row h-100">
+                            <h1 class="my-auto">
+                                <span class="d-block text-light">Previous Projects </span>
+                            </h1>
+                        </div>
+                    </div>
+
+
+                </div>
+                <!-- <div class="row">
+                    <div class="col-12 h-100">
+                        <div class="px-5 py-5 py-lg-3 text-center my-auto">
+                            <p class="lead mt-3 fs-4 text-secondary">Participating in SPARK will be an exciting and
+                                rewarding proposition for students at ENTC. Showing courage and leadership they will
+                                be expected to solve some of the most intractable challenges facing humanity. Working
+                                in teams over a period of a year they will explore the boundaries of their capabilities
+                                and at pace develop innovative solutions to resolve those challenges. They will sharpen
+                                their communications, presentation, social media influencing skills in presenting their
+                                valuable experience to an external audience. </p>
+                            <p class="lead mb-3 fs-4 text-secondary">
+                                The World Economic Forum (WEF) has identified a number of soft skills required to effectively
+                                participate in modern commerce and industry. In participating in the program, the undergraduates
+                                will reinforce or acquire new skills that have been highlighted by the WEF.
+                            </p>
+                        </div>
+                    </div>
+                </div> -->
+
+
+        </header>
+
+
+
+        <section class="testimonial section-padding pt-0">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-12 text-center">
+                        <h2 class="mb-5">Featured Products</h2>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-12 mb-3">
+                        <div class="product-thumb h-100">
+                            <a href="#" class="product-title-link">
+                                <div class="product-info d-flex">
+                                    <div>
+                                        <h5 class="product-title mb-0">
+                                            Tesseract
+                                        </h5>
+                                        <p class="product-p">Medical Waste Segregation Using AI</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-12 mb-3">
+                        <div class="product-thumb h-100">
+                            <a href="#" class="product-title-link">
+                                <div class="product-info d-flex">
+                                    <div>
+                                        <h5 class="product-title mb-0">
+                                            Techstatic
+                                        </h5>
+
+                                        <p class="product-p">Innovating Agriculture for Sustainability</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-12 mb-3">
+
+                        <div class="product-thumb h-100">
+                            <a href="#" class="product-title-link">
+                                <div class="product-info d-flex">
+                                    <div>
+                                        <h5 class="product-title mb-0">
+                                            PestSense
+                                        </h5>
+
+                                        <p class="product-p">A Sustainable Solution for Insect Management</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-12 mb-3">
+
+                        <div class="product-thumb h-100">
+                            <a href="#" class="product-title-link">
+                                <div class="product-info d-flex">
+                                    <div>
+                                        <h5 class="product-title mb-0">
+                                            Amicis
+                                        </h5>
+
+                                        <p class="product-p">Environmental Conservation Through NBOT</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-12 mb-3">
+
+                        <div class="product-thumb h-100">
+                            <a href="#" class="product-title-link">
+                                <div class="product-info d-flex">
+                                    <div>
+                                        <h5 class="product-title mb-0">
+                                            Green Algae
+                                        </h5>
+
+                                        <p class="product-p">Revolutionizing Hydrogen Production for a Sustainable Future</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-12 mb-3">
+
+                        <div class="product-thumb h-100">
+                            <a href="#" class="product-title-link">
+                                <div class="product-info d-flex">
+                                    <div>
+                                        <h5 class="product-title mb-0">
+                                            Revolutionizing Indoor Cycling
+                                        </h5>
+
+                                        <p class="product-p">A 5G Powered virtual cycling platform</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <!-- <div class="col-12 text-center">
+                        <a href="products.html" class="view-all">View All Products</a>
+                    </div> -->
+
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-10 me-auto mb-4">
+                    <h4 class="text-white mb-3"><a href="index.html">SPARK</a> Challenge</h4>
+                    <p class="copyright-text text-muted mt-lg-5 mb-4 mb-lg-0">Copyright © 2023 <strong>ENTC</strong></p>
+                    <br>
+                    <p class="copyright-text">Designed by <a href="https://www.linkedin.com/in/nidula-gunawardana-22819a1b3/" target="_blank">Nidula Gunawardana</a></p>
+                </div>
+
+                <div class="col-lg-5 col-8">
+                    <h5 class="text-white mb-3">Sitemap</h5>
+
+                    <ul class="footer-menu d-flex flex-wrap">
+                        <li class="footer-menu-item"><a href="index.php" class="footer-menu-link">Home</a></li>
+
+                        <li class="footer-menu-item"><a href="raspberryPi.php" class="footer-menu-link">Raspberry Pi Community</a></li>
+
+                        <li class="footer-menu-item"><a href="communityFb.php" class="footer-menu-link">Community Facebook Page</a></li>
+
+                        <li class="footer-menu-item"><a href="softSkills.php" class="footer-menu-link">Soft Skills</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-4">
+                    <h5 class="text-white mb-3">Social</h5>
+
+                    <ul class="social-icon">
+
+                        <li><a href="#" class="social-icon-link bi-youtube"></a></li>
+
+                        <li><a href="#" class="social-icon-link bi-whatsapp"></a></li>
+
+                        <li><a href="#" class="social-icon-link bi-instagram"></a></li>
+
+                        <li><a href="#" class="social-icon-link bi-skype"></a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+
+    <!-- JAVASCRIPT FILES -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/Headroom.js"></script>
+    <script src="js/jQuery.headroom.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
